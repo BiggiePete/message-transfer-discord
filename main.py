e015a858892445d48af864cbf8cd239057d1a0bc
Vijay Stroup <vijaystroup@gmail.com>
2021-02-18 17:30:33 -0500

    update on_ready event

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
+import os
 import discord
 from discord.ext import commands
 
-# bot = commands.Bot(command_prefix=f'<@!{811718334583930970}>')
-bot = commands.Bot(command_prefix=f'.')
+bot = commands.Bot(command_prefix=f'<@!{811718334583930970}> ')
 
 
 @bot.event
 async def on_ready():
+    """When bot is first started"""
+
+    # add all extensions
+    for filename in os.listdir(os.path.join(os.path.dirname(__file__), 'cogs')):
+        if filename.endswith('.py'):
+            bot.load_extension(f'cogs.{filename[:-3]}')
+    
+    # change bot presence
+    await bot.change_presence(activity=discord.Game(f'KEKW'))
+
     print(f'Logged in as {bot.user}')
-
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith('ping'):
-        await message.channel.send('pong!')
-
-
-@bot.command()
-async def test(ctx, arg):
-    print('HERE')
-    await ctx.send(arg)
 
 
 bot.run('ODExNzE4MzM0NTgzOTMwOTcw.YC2Rmw.BLQuxqSiZ20mXLYdkf7r_hC9L_A')
