6d0ffd6391c23f24e1a230ad229614023702d2ae
Vijay Stroup <vijaystroup@gmail.com>
2021-02-23 18:28:56 -0500

    deleted embed message

--- cogs/logs.py
+++ cogs/logs.py
@@ -1,7 +1,6 @@
 import datetime
 import discord
 from discord.ext import commands
-from discord.utils import get
 
 
 class Logs(commands.Cog):
@@ -9,12 +8,21 @@ class Logs(commands.Cog):
         self.bot = bot
         self.log_channel = bot.get_channel(812408474511474709)
     
-    def make_embed(self):
+    @staticmethod
+    def make_embed(title: str, author: str, channel: str, _id: tuple, message: str):
         """Make embed template used by different events"""
 
-        embed = discord.Embed(title="Title", description="Desc", color=0x00ff00)
-        embed.add_field(name="Field1", value="hi", inline=False)
-        embed.add_field(name="Field2", value="hi2", inline=False)
+        time = datetime.datetime.utcnow() - datetime.timedelta(hours=5)
+
+        embed = discord.Embed(
+            title=title,
+            description=str(time),
+            color=discord.Color.dark_red()
+        )
+        embed.add_field(name='Author', value=author, inline=True)
+        embed.add_field(name='Channel', value=channel, inline=True)
+        embed.add_field(name=_id[0], value=_id[1], inline=True)
+        embed.add_field(name='Message', value=message[:1024], inline=False)
 
         return embed
 
@@ -22,21 +30,39 @@ class Logs(commands.Cog):
     async def on_raw_message_delete(self, payload: discord.RawMessageDeleteEvent):
         """Log deleted messages to log channel"""
 
-        time = datetime.datetime.utcnow() - datetime.timedelta(hours=5)
 
         if payload.cached_message:
-            author = payload.cached_message.author
-            channel = payload.cached_message.channel
+            message = ''
 
-            embed = discord.Embed(
+            if payload.cached_message.content:
+                message += f'message: {payload.cached_message.content}\n\n'
+
+            if payload.cached_message.embeds:
+                for embed in payload.cached_message.embeds:
+                    message += f'embed: {str(embed.to_dict())}\n'
+                message += '\n'
+
+            if payload.cached_message.attachments:
+                pass
+
+            if payload.cached_message.stickers:
+                pass
+
+            embed = self.make_embed(
                 title='Deleted Message',
-                description=str(time),
-                color=discord.Color.red()
+                author=payload.cached_message.author.mention,
+                channel=payload.cached_message.channel.mention,
+                _id=('Message ID', payload.message_id),
+                message=message
             )
-            embed.add_field(name='Author', value=author.mention, inline=True)
-            embed.add_field(name='Channel', value=channel.mention, inline=True)
-            embed.add_field(name='Message ID', value=payload.message_id, inline=True)
-            embed.add_field(name='Message', value=payload.cached_message.content, inline=False)
+        else:
+            embed = self.make_embed(
+                title='Deleted Message',
+                author='Unknown',
+                channel=self.bot.get_channel(payload.channel_id).mention,
+                _id=('Message ID', payload.message_id),
+                message='`Message not in cache`'
+            )
 
         await self.log_channel.send(embed=embed)
 
