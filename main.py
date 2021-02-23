4800608277ffc19725e5de7c1a2b245c161464f3
Vijay Stroup <vijaystroup@gmail.com>
2021-02-22 23:04:13 -0500

    change activity at creating instance

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import discord
 from discord.ext import commands
 
 # bot = commands.Bot(command_prefix=f'<@!{811718334583930970}> ')
-bot = commands.Bot(command_prefix='.')
+bot = commands.Bot(command_prefix='.', activity=discord.Game(f'KEKW'))
 
 
 @bot.event
@@ -14,11 +14,7 @@ async def on_ready():
     for filename in os.listdir(os.path.join(os.path.dirname(__file__), 'cogs')):
         if filename.endswith('.py'):
             bot.load_extension(f'cogs.{filename[:-3]}')
-    
-    # change bot presence
-    await bot.change_presence(activity=discord.Game(f'KEKW'))
 
     print(f'Logged in as {bot.user}')
-
 
 bot.run('ODExNzE4MzM0NTgzOTMwOTcw.YC2Rmw.BLQuxqSiZ20mXLYdkf7r_hC9L_A')
