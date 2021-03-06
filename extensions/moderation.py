5eb4f645ec779a6ef63934dd23a345a03a8bb545
Vijay Stroup <vijaystroup@gmail.com>
2021-03-06 14:59:39 -0500

    notyping command

--- extensions/moderation.py
+++ extensions/moderation.py
@@ -1,4 +1,6 @@
+import datetime
 from typing import Optional
+import discord
 from discord.ext import commands
 from cfg import cfg
 
@@ -7,7 +9,11 @@ class Moderation(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-        self.blacklist_categories = [
+        self.red = discord.Color.from_rgb(255, 0, 0)
+        self.orange = discord.Color.from_rgb(255, 150, 0)
+        self.black = discord.Color.from_rgb(0, 0, 0)
+
+        self.purge_blacklist_categories = [
             # 805217167847063573, # Register
             # 804825493856976906, # Information
             # 804829689033785344, # Game Logs
@@ -19,8 +25,18 @@ class Moderation(commands.Cog):
     async def purge(self, ctx: commands.Context, n: Optional[int]=1):
         """Purge chat messages"""
 
-        if ctx.channel.category_id not in self.blacklist_categories:
+        meta = {
+            'title': 'Moderation Command Executed',
+            'color': self.red
+        }
+
+        if ctx.channel.category_id not in self.purge_blacklist_categories:
             await ctx.channel.purge(limit = n+1)
+            await cfg['moderation_channel'].send(embed=await self.make_moderation_embed(
+                meta=meta,
+                admin=ctx.author,
+                command='purge'
+            ))
 
     @purge.error
     async def purge_error(self, ctx: commands.Context, error: commands.CommandError):
@@ -30,6 +46,60 @@ class Moderation(commands.Cog):
             return
 
         await ctx.send(f'Error executing purge:\n`{error}`', delete_after=10)
+
+    @commands.command()
+    @commands.has_role(cfg['administration_spacer'].id)
+    async def notyping(self, ctx: commands.Context, member: discord.Member):
+        """Gives member No Typing role to prevent member from sending text
+        messages
+        """
+
+        meta = {
+            'title': 'Moderation Command Executed',
+            'color': self.red
+        }
+
+        await member.add_roles(cfg['no_typing_role'], cfg['trouble_spacer'])
+        await cfg['moderation_channel'].send(embed=await self.make_moderation_embed(
+            meta=meta,
+            admin=ctx.author,
+            command='notyping',
+            target=member
+        ))
+
+    @notyping.error
+    async def notyping_error(self, ctx: commands.Context, error: commands.CommandError):
+        """Function executed when there was an error associated with notyping"""
+
+        if isinstance(error, commands.MissingRole):
+            return
+
+        await ctx.send(f'Error executing notyping:\n`{error}`', delete_after=10)
+
+    @staticmethod
+    async def make_moderation_embed(
+        meta: dict,
+        admin: discord.Member,
+        command: str,
+        target: Optional[discord.Member]=None,
+    ) -> discord.Embed:
+        """Return embed for logging moderation commands"""
+
+        embed = discord.Embed(
+            title=meta.get('title'),
+            color=meta.get('color'),
+            timestamp=datetime.datetime.utcnow()
+        )
+        embed.set_thumbnail(url=admin.avatar_url)
+        embed.add_field(name='Admin', value=admin.mention, inline=True)
+        embed.add_field(name='ID', value=admin.id, inline=True)
+        embed.add_field(name='Command', value=command, inline=False)
+
+        if target:
+            embed.add_field(name='Target', value=target.mention, inline=True)
+            embed.add_field(name='ID', value=target.id, inline=True)
+
+        return embed
 
 
 def setup(bot: commands.Bot):
