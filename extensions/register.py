95cda9800f4160825cc9c9041c609aad41330dbf
Vijay Stroup <vijaystroup@gmail.com>
2021-03-06 14:07:22 -0500

    register message command

--- extensions/register.py
+++ extensions/register.py
@@ -7,11 +7,23 @@ class Register(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    @commands.command()
+    @commands.has_role(cfg['owner_role'].id)
+    async def wlmsg(self, ctx: commands.Context):
+        """Make message that users must agree with to be whitelisted"""
+
+        await ctx.message.delete()
+        msg = await ctx.channel.send(
+            f'After you have read the rules, react to this message with '
+            f'{cfg["emojis"]["yes"]["full"]} to be whitelisted.'
+        )
+        await msg.add_reaction(cfg['emojis']['yes']['full'])
+
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
         """Add whitelist role after accepting rules"""
 
-        if payload.message_id == cfg['rules_message_id']:
+        if payload.message_id == cfg['whitelist_message_id']:
             if payload.emoji.id == cfg['emojis']['yes']['id']:
                 try:
                     await payload.member.add_roles(
