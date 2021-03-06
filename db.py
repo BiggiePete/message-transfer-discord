55d0309f57c68501b1361955c4c0967e09ae1707
Vijay Stroup <vijaystroup@gmail.com>
2021-03-06 17:18:33 -0500

    database class

---
+++ db.py
@@ -0,0 +1,50 @@
+import sqlite3
+import discord
+
+
+class DB:
+    def __init__(self):
+        self.connection = sqlite3.connect('discord.db')
+        self.c = self.connection.cursor()
+
+    def init(self):
+        """"Make tables for db"""
+
+        self.c.execute('''
+            CREATE TABLE IF NOT EXISTS members (
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL,
+                discriminator TEXT NOT NULL,
+                discord_id INTEGER,
+                joined_guild DATE NOT NULL,
+                points INTEGER NOT NULL
+            );
+        ''')
+        self.connection.commit()
+
+    def purge(self):
+        """Drop all tables and data"""
+
+        self.c.execute('''
+            DROP TABLE *;
+        ''')
+        self.connection.commit()
+
+    def new_member(self, member: discord.Member):
+        """Create new entry in members table of member"""
+
+        self.c.execute('''
+            INSERT INTO members (name, discriminator, discord_id, joined_guild, points)
+            VALUES (?, ?, ?, ?, ?);
+        ''', (
+            member.name,
+            member.discriminator,
+            member.id,
+            member.joined_at,
+            0
+        ))
+
+        self.connection.commit()
+    
+    # def get_member(self, member: discord.Member):
+    #     """Return member from db referenced from discord.Member"""
