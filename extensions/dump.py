71d627c62be8d08b5053eb510e45f17ae7512ce0
Vijay Stroup <vijaystroup@gmail.com>
2021-09-01 20:09:44 -0400

    dump to csv file

--- extensions/dump.py
+++ extensions/dump.py
@@ -4,12 +4,18 @@ from cfg import cfg
 class Dump(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+        self.delimiter = '\t'
 
     @commands.command()
     async def dump(self, ctx: commands.Context, filename: str):
         """Dump message history to csv file"""
 
-        await ctx.reply('test')
+        with open(f'{filename}.csv', 'w') as f:
+            f.write(f'content{self.delimiter}created_at{self.delimiter}pinned\n')
+            async for message in ctx.channel.history(limit=None, oldest_first=True):
+                f.write(f'{message.content}{self.delimiter}{message.created_at}{self.delimiter}{message.pinned}\n')
+
+        await ctx.reply('done')
 
     @dump.error
     async def dump_error(self, ctx: commands.Context, error: commands.CommandError):
